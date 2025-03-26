3c693037ad85c3c43d8d16a8966a9da49c504b83
AhmedIBRAHIM01 <ahmedibraiii2000@gmail.com>
2025-03-26 23:36:45 +0100

    test: add test to AfterConnectionClosed methode

--- src/test/java/at/aau/serg/monopoly/websoket/GameWebSocketHandlerUnitTest.java
+++ src/test/java/at/aau/serg/monopoly/websoket/GameWebSocketHandlerUnitTest.java
@@ -3,12 +3,11 @@ package at.aau.serg.monopoly.websoket;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -42,8 +41,42 @@ class GameWebSocketHandlerUnitTest {
     }
 
     @Test
-    void afterConnectionClosed() {
+    void testAfterConnectionClosed() throws Exception{
+        gameWebSocketHandler.afterConnectionClosed(session, CloseStatus.NORMAL);
+        verify(session, never()).sendMessage(any());
+
     }
+
+    @Test
+    void testAfterConnectionClosedWithRemainingSessions() throws IOException {
+
+        WebSocketSession session2 = mock(WebSocketSession.class);
+
+
+        when(session.getId()).thenReturn("1");
+        when(session2.getId()).thenReturn("2");
+
+
+        when(session.isOpen()).thenReturn(true);
+        when(session2.isOpen()).thenReturn(true);
+
+
+        gameWebSocketHandler.afterConnectionEstablished(session);
+        gameWebSocketHandler.afterConnectionEstablished(session2);
+
+
+        gameWebSocketHandler.afterConnectionClosed(session, CloseStatus.NORMAL);
+
+
+        verify(session, times(1)).sendMessage(new TextMessage("Player joined: 1 (Total: 1)"));
+        verify(session2, times(1)).sendMessage(new TextMessage("Player joined: 2 (Total: 2)"));
+        verify(session, times(1)).sendMessage(new TextMessage("Player joined: 2 (Total: 2)"));
+        verify(session2, times(1)).sendMessage(new TextMessage("Player left: 1 (Total: 1)"));
+    }
+
+
+
+
 
     @AfterEach
     void tearDown() {
