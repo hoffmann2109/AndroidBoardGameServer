00812c42b947e0f1422774b3466cec804b147b89
AhmedIBRAHIM01 <ahmedibraiii2000@gmail.com>
2025-03-26 20:51:53 +0100

    test: add unit test for afterConnectionEstablished method

---
+++ src/test/java/at/aau/serg/monopoly/websoket/GameWebSocketHandlerUnitTest.java
@@ -0,0 +1,48 @@
+package at.aau.serg.monopoly.websoket;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class GameWebSocketHandlerUnitTest {
+
+    private WebSocketSession session;
+    private GameWebSocketHandler gameWebSocketHandler;
+
+    @BeforeEach
+    void setUp() {
+        gameWebSocketHandler= new GameWebSocketHandler();
+        session= mock(WebSocketSession.class);
+        when(session.getId()).thenReturn("1");
+    }
+
+
+
+    @Test
+    void testAfterConnectionEstablished() throws IOException {
+        gameWebSocketHandler.afterConnectionEstablished(session);
+        verify(session,  times(1)).sendMessage(new TextMessage("Player joined: 1 (Total: 1)"));
+    }
+
+    @Test
+    void handleTextMessage() {
+    }
+
+    @Test
+    void afterConnectionClosed() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        gameWebSocketHandler=null;
+        session= null;
+    }
+}
