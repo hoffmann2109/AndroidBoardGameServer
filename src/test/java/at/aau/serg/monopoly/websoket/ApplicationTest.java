8f3f184427c4b7e67b4781d61994b6ec469b620e
AhmedIBRAHIM01 <ahmedibraiii2000@gmail.com>
2025-03-27 02:47:39 +0100

    test: add unit test for Application methode

---
+++ src/test/java/at/aau/serg/monopoly/websoket/ApplicationTest.java
@@ -0,0 +1,14 @@
+package at.aau.serg.monopoly.websoket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+        Application.main(new String[]{});
+    }
+}
