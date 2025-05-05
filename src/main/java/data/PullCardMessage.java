6b8f68c4950a9213d70c6c56f8152839da7ebe12
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-05-05 09:37:48 +0200

    Added a DTO for incoming PullCardMessages

---
+++ src/main/java/data/PullCardMessage.java
@@ -0,0 +1,8 @@
+package data;
+
+public class PullCardMessage {
+    public String type;     // == "PULL_CARD"
+    public String playerId;
+    public String cardType; // COMMUNITY_CHEST or CHANCE
+}
+
