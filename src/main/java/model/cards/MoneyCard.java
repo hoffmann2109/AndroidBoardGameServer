e4e3cadbb2bbdf7e936d8e3d05ae85d5fe9287d5
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-05-05 11:14:17 +0200

    Implemented GET_MONEY and PAY messages

--- src/main/java/model/cards/MoneyCard.java
+++ src/main/java/model/cards/MoneyCard.java
@@ -3,6 +3,8 @@ package model.cards;
 import model.Game;
 import model.Player;
 
+import java.util.List;
+
 public class MoneyCard extends Card {
     private int amount;
     private boolean othersPay;   // you get money from all other players if true
@@ -10,6 +12,31 @@ public class MoneyCard extends Card {
 
     @Override
     public void apply(Game game, Player player) {
+        String me = player.getId();
+        List<Player> all = game.getPlayers();
 
+        // Each player pays some amount to me
+        if (othersPay) {
+            for (Player p : all) {
+                if (!p.getId().equals(me)) {
+                    game.updatePlayerMoney(p.getId(), -amount);
+                    game.updatePlayerMoney(me, amount);
+                }
+            }
+
+        } else if (othersGet) { // I have to pay some amount to all other players
+            for (Player p : all) {
+                if (!p.getId().equals(me)) {
+                    game.updatePlayerMoney(p.getId(), amount);
+                    game.updatePlayerMoney(me, -amount);
+                }
+            }
+
+        } else if (getAction() == ActionType.GET_MONEY) { // Simple bank payout
+            game.updatePlayerMoney(me, amount);
+
+        } else { // Simple bank payment
+            game.updatePlayerMoney(me, -amount);
+        }
     }
 }
