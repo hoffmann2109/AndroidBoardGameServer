b5f4f53c8a839c9cc57579e096dbc5a9edc03563
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-05-21 10:16:12 +0200

    Finished class setup: set constants for the different methods and a generic getAmount method

--- src/main/java/at/aau/serg/monopoly/websoket/CheatService.java
+++ src/main/java/at/aau/serg/monopoly/websoket/CheatService.java
@@ -1,15 +1,35 @@
 package at.aau.serg.monopoly.websoket;
 
 import org.springframework.stereotype.Service;
-
+import java.security.SecureRandom;
 import java.util.Locale;
 
 @Service
 public class CheatService {
     private int amount;
+    private final SecureRandom rnd = new SecureRandom();
+    private final int FIXED_MONEY_DELTA = 250;
+    private final int RANDOM_MONEY_CEILING = 1000;
+    private final int RANDOM_MONEY_STEP = 50;
+    private final int COINFLIP_AMOUNT = 500;
 
     public int getAmount(String cheatCode, int currentMoney){
-        String cheatCodeTypeSafe = cheatCode
+        String cheatCodeTypeSafe = normalizeInput(cheatCode);
+
+        switch (cheatCodeTypeSafe){
+            case "nevergonnagiveyouup": return fixedExtraMoney();
+            case "nevergonnaletyoudown": return randomMoney();
+            case "gamblingaddiction": return coinflip();
+            case "yolo": return doubleOrHalf(currentMoney);
+            default: return 0;
+        }
+    }
+
+    // INFO: Every method returns a delta and not the new amount
+
+    // Method should be private but I wanted to test it
+    public String normalizeInput(String cheatCode){
+        return cheatCode
                 .trim()
                 .toLowerCase(Locale.ROOT)
                 .replaceAll("\\s+", "");
