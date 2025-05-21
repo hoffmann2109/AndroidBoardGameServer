4485d988040a20922dbce3622cd1d7f28ff3d9d2
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-05-21 08:50:05 +0200

    Created a CheatService class with a minimal implementation

---
+++ src/main/java/at/aau/serg/monopoly/websoket/CheatService.java
@@ -0,0 +1,10 @@
+package at.aau.serg.monopoly.websoket;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheatService {
+    public int getAmount(String cheatCode){
+        return 500;
+    }
+}
