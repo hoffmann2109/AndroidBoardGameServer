6baabe5f4ed5017c24b326fc4bbdbbe71f0b4aa2
Daniel Buttazoni <daniel.buttazoni@hotmail.com>
2025-05-25 15:16:58 +0200

    removed unused imports

--- src/test/java/at/aau/serg/monopoly/websoket/RentCollectionServiceTest.java
+++ src/test/java/at/aau/serg/monopoly/websoket/RentCollectionServiceTest.java
@@ -1,12 +1,10 @@
 package at.aau.serg.monopoly.websoket;
 
 import model.Player;
-import model.properties.BaseProperty;
 import model.properties.HouseableProperty;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.MockitoAnnotations;
