4371b4d4d24157a836342258558987928b54d9ae
lendlerm <lendlerm@edu.htl-villach.at>
2025-06-17 23:01:50 +0200

    Removed unused import

--- src/test/java/at/aau/serg/monopoly/websoket/GameWebSocketHandlerTaxTest.java
+++ src/test/java/at/aau/serg/monopoly/websoket/GameWebSocketHandlerTaxTest.java
@@ -11,7 +11,6 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
